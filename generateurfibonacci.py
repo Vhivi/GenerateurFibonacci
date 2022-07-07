f841d31d26f35e9ceffcd666eb8a3431d4968805
Vhivi <david.angelelli@hotmail.fr>
2022-07-07 11:33:43 +0200

    Vérification exigences PEP8

--- generateurfibonacci.py
+++ generateurfibonacci.py
@@ -4,9 +4,9 @@
 # Générateur des nombres de la suite de Fibonacci
 
 # La série mathématique connue sous le nom de suite de Fibonacci a été l’une
-# des questions informatique les plus populaires. Essentiellement, vous 
-# commencez avec deux nombres, de préférence 0 et 1, et vous les ajoutez 
-# pour créer votre troisième nombre de Fibonacci. Ensuite, il suffit 
+# des questions informatique les plus populaires. Essentiellement, vous
+# commencez avec deux nombres, de préférence 0 et 1, et vous les ajoutez
+# pour créer votre troisième nombre de Fibonacci. Ensuite, il suffit
 # d’additionner la somme et l’avant-dernier terme de Fibonacci pour générer
 # le suivant.
 
@@ -17,8 +17,8 @@
 # Dans ce projet, vous demandez la position du nombre de Fibonacci requise
 # par l’utilisateur et vous le générez simplement. Une fois généré, vous
 # pouvez afficher le nombre correspondant à l’utilisateur. Vous pouvez aller
-# plus loin et montrer à l’utilisateur la série entière jusqu’à ce point avec 
-# le fonctionnement mathématique. C’est l’un des meilleurs projets Python 
+# plus loin et montrer à l’utilisateur la série entière jusqu’à ce point avec
+# le fonctionnement mathématique. C’est l’un des meilleurs projets Python
 # pour s’initier au concept de la fonction récursive.
 
 
@@ -33,16 +33,19 @@ def fibonacci(position):
         b = total
         total = a + b
         suitelist.append(total)
-    
+
     return suitelist[position - 1], suitelist
+
 
 def main():
     # Demande de la position du nombre
-    position = int(input('''Quel position dans la suite de Fibonacci 
+    position = int(input('''Quel position dans la suite de Fibonacci
                          voulez-vous voir ?'''))
-    
+
     # Génération de la suite et affichage dans la console
-    print('Le nombre à la position {} est {}'.format(position, fibonacci(position)[0]))
-    print('Voici la suite entière jusqu\'à la position demandée : {}'.format(fibonacci(position)[1]))
+    print('Le nombre à la position {} est {}'
+          .format(position, fibonacci(position)[0]))
+    print('Voici la suite entière jusqu\'à la position demandée : {}'
+          .format(fibonacci(position)[1]))
 
 main()
